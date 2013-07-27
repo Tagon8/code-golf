f5432eb149a1d11cdf56dab32861d374369af82a
Kaique da Silva kaique.developer@gmail.com <kaique.developer@gmail.com>
2013-07-27 15:59:59 -0300

    Added source files stop in 255

--- roman-to-decimal/index.js
+++ roman-to-decimal/index.js
@@ -37,12 +37,12 @@
             }
         }
 
-        if ( expression.search(/^([LCX][XL]{1,})+(I[V|X])$/) > -1 ) out -= 2;
+        if ( expression.search(/^([LCX][CXL]{1,})+(I[V|X])$/) > -1 ) out -= 2;
         if ( expression.search(/^[XLCDM]IV$/) > -1 ) out -= 2;
         if ( expression.search(/^[XLCDM]IX$/) > -1 ) out += 18;
         if ( expression.search(/^[LCDMX]X/) > -1 ) out += 20;
-        if ( expression.search(/^[C][L]/) > -1 ) out += 100;
-        if ( expression.search(/^[C]{2,}/) > -1 ) out += 200;
+        if ( expression.search(/^[C][L]/) > -1 ) out += token['C'];
+        if ( expression.search(/^[C]{2,}/) > -1 ) out += token['C'] * 2;
 
         return out;
     };
